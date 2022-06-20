8b449270ff7e402c82b112582526831c75a20fb8
Alex Andrieiev <andryeyevalex@gmail.com>
2022-06-20 22:05:58 +0300

    Add system files

---
+++ js/practice.js
@@ -0,0 +1,20 @@
+// let a = prompt('Давай число');
+// a = Number(a);
+// console.log(a);
+
+// let b = prompt('Давай степінь');
+// b = Number(b);
+// console.log(b);
+
+// const sum = a ** b;
+// console.log(sum);
+
+// const max = 50;
+// const min = 40;
+// const result = Math.round(Math.random() * (max - min) + min);
+
+// console.log(result);
+
+let number = Number.parseInt("25.6px");
+
+console.log(number);
