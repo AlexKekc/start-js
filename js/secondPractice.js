c60422e98d8204c6a71107329837ced68e7a02b2
Alex Andrieiev <andryeyevalex@gmail.com>
2022-07-17 20:33:54 +0300

    Add examples with callback functions

--- js/secondPractice.js
+++ js/secondPractice.js
@@ -1,3 +1,438 @@
-let a = prompt('Давай число');
-a = Number(a);
-console.log(a);
+/* Замикання функцій */
+
+// let val = 7;
+// function createAdder() {
+//   function addNumbers(a, b) {
+//     let ret = a + b;
+//     return ret;
+//   }
+//   return addNumbers;
+// }
+// let adder = createAdder();
+// let sum = adder(val, 8);
+
+// console.log('example of function returning a function: ', sum);
+
+// function createCounter() {
+//   let counter = 0;
+//   const myFunction = function () {
+//     counter = counter + 1;
+//     return counter;
+//   };
+//   return myFunction;
+// }
+// const increment = createCounter();
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+// console.log('example increment', c1, c2, c3);
+
+// let c = 4;
+// const addX = x => n => n + x;
+// const addThree = addX(3);
+// let d = addThree(c);
+// console.log('example partial application', d);
+
+/* Метод forEach */
+// const users = [
+//   { name: 'Alex', age: 30, hair: 'black' },
+//   { name: 'Michael', age: 40, hair: 'white' },
+//   { name: 'Den', age: 35, hair: 'blue' },
+// ];
+
+// function forEach(array, callback) {
+//   for (let i = 0; i < array.length; i += 1) {
+//     callback(array[i]);
+//   }
+//   return array;
+// }
+
+// function getName({ name, age, hair }) {
+//   console.log('name', name);
+//   console.log('age', age);
+//   console.log('hair', hair);
+// }
+
+// console.log(forEach(users, getName));
+
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   // Change code below this line
+
+//   // for (let i = 0; i < firstArray.length; i += 1) {
+//   //   if (secondArray.includes(firstArray[i])) {
+//   //     commonElements.push(firstArray[i]);
+//   //   }
+//   // }
+
+//   firstArray.forEach(function (element) {
+//     if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+//   });
+
+//   return commonElements;
+//   // Change code above this line
+// }
+
+// console.log(getCommonElements([1, 2, 3, 4, 5], [7, 8, 9, 4, 5, 10, 11]));
+
+// function changeEven(numbers, value) {
+//   // Change code below this line
+//   let newArray = [];
+
+//   numbers.forEach(element => {
+//     if (element % 2 === 0) {
+//       newArray.push(element + value);
+//     }
+//   });
+
+//   return newArray;
+//   // Change code above this line
+// }
+
+// console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
+
+// filter
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+
+// flatMap, filter
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     genres: ['adventure', 'history'],
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     genres: ['fiction', 'mysticism'],
+//   },
+//   {
+//     title: 'Redder Than Blood',
+//     author: 'Tanith Lee',
+//     genres: ['horror', 'mysticism', 'adventure'],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap(book => book.genres);
+// const uniqueGenres = allGenres.filter(
+//   (genres, index, array) => array.indexOf(genres) === index
+// );
+
+// console.log(allGenres);
+// console.log(uniqueGenres);
+
+const users = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+    age: 37,
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+    age: 34,
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+    age: 24,
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+    age: 21,
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+    age: 27,
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+    age: 38,
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+    age: 39,
+  },
+];
+
+/* Дополни функцию getFriends(users) так, чтобы она возвращала массив друзей всех пользователей (свойство friends). 
+У нескольких пользователей могут быть одинаковые друзья, сделай так чтобы возвращаемый массив не содержал повторений. */
+// const getFriends = users =>
+//   users
+//     .flatMap(user => user.friends)
+//     .filter((friend, index, array) => array.indexOf(friend) === index);
+
+// console.log(getFriends(users));
+
+/* Дополни функцию getActiveUsers(users) так, чтобы она возвращала массив активных пользователей, 
+значение свойства isActive которых true. */
+// const getActiveUsers = users =>
+//   users.filter(({ isActive }) => (isActive ? isActive : 0));
+
+// console.log(getActiveUsers(users));
+
+/* Дополни функцию getInactiveUsers(users) так, чтобы она возвращала массив неактивных пользователей, 
+значение свойства isActive которых false. */
+// const getInactiveUsers = users =>
+//   users.filter(({ isActive }) => (!isActive ? !isActive : 0));
+
+// console.log(getInactiveUsers(users));
+
+/* Нашему сервису необходимо рассчитать среднее время проведённое в одной игре для каждого игрока, 
+и получить общую сумму этих времён. Рассчитать время для каждого из игроков, можно разделив 
+его время (свойство playtime) на количество игр (свойство gamesPlayed). */
+// const players = [
+//   { name: 'Mango', playtime: 1270, gamesPlayed: 4 },
+//   { name: 'Poly', playtime: 469, gamesPlayed: 2 },
+//   { name: 'Ajax', playtime: 690, gamesPlayed: 3 },
+//   { name: 'Kiwi', playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+// const averagePlayTime = players.playtime / players.gamesPlayed;
+
+// const totalAveragePlaytimePerGame = players.reduce((total, { playtime, gamesPlayed }) => {
+//   return total + playtime / gamesPlayed;
+// }, 0);
+
+/* Дополни функцию calculateTotalBalance(users) так, чтобы она считала и возвращала сумму 
+всех средств (свойство balance) которые хранят пользователи из массива users. */
+// const calculateTotalBalance = users =>
+//   users.reduce((total, { balance }) => total + balance, 0);
+
+// console.log(calculateTotalBalance(users));
+
+/* Дополни функцию getTotalFriendCount(users) так, чтобы она считала и возвращала общее 
+количество друзей (свойство friends) всех пользователей из массива users. */
+// const getTotalFriendCount = users =>
+//   users.reduce((total, { friends }) => total + friends.length, 0);
+
+// console.log(getTotalFriendCount(users));
+
+/* Дополни код так, чтобы в переменной ascendingReleaseDates получилась отсортированная по возрастанию 
+копия массива releaseDates, а в переменной alphabeticalAuthors копия массива имён авторов authors 
+отсортированная в по алфавиту. */
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//   'Tanith Lee',
+//   'Bernard Cornwell',
+//   'Robert Sheckley',
+//   'Fyodor Dostoevsky',
+// ];
+// // Change code below this line
+
+// const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
+
+// const alphabeticalAuthors = [...authors].sort((a, b) => a.localeCompare(b));
+
+/* Онлайн бибилиотеке необходимо отображать книги сортированные по дате издания, по её возрастанию или убыванию.
+ Дополни код так, чтобы в переменной ascendingReleaseDates получилась отсортированная по возрастанию копия 
+ массива releaseDates, а в переменной descendingReleaseDates копия отсортированная по убыванию. */
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+
+// const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
+
+// const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
+
+// console.log(ascendingReleaseDates);
+// console.log(descendingReleaseDates);
+
+/* Онлайн бибилиотеке необходимо отображать книги отсортированные по автору, в алфавитном и обратном 
+алфавитном порядке. Дополни код так, чтобы в переменной authorsInAlphabetOrder получилась отсортированная 
+по алфавиту копия массива authors, а в переменной authorsInReversedOrder копия отсортированная в обратном 
+алфавитном порядке. */
+// const authors = [
+//   'Tanith Lee',
+//   'Bernard Cornwell',
+//   'Robert Sheckley',
+//   'Fyodor Dostoevsky',
+//   'Howard Lovecraft',
+// ];
+
+// const authorsInAlphabetOrder = [...authors].sort((a, b) => a.localeCompare(b));
+
+// const authorsInReversedOrder = [...authors].sort((a, b) => b.localeCompare(a));
+
+// console.log(authorsInAlphabetOrder);
+// console.log(authorsInReversedOrder);
+
+/* Дополни код так, чтобы:
+В переменной sortedByAuthorName получился массив книг отсортированный по имени автора в алфавитном порядке.
+В переменной sortedByReversedAuthorName получился массив книг отсортированный по имени автора в обратном алфавитном порядке.
+В переменной sortedByAscendingRating получился массив книг отсортированный по возрастанию рейтинга.
+В переменной sortedByDescentingRating получился массив книг отсортированный по убыванию рейтинга. */
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const sortedByAuthorName = [...books].sort((firstBook, secondBook) =>
+//   firstBook.author.localeCompare(secondBook.author)
+// );
+
+// const sortedByReversedAuthorName = [...books].sort((firstBook, secondBook) =>
+//   secondBook.author.localeCompare(firstBook.author)
+// );
+
+// const sortedByAscendingRating = [...books].sort(
+//   (firstBook, secondBook) => firstBook.rating - secondBook.rating
+// );
+
+// const sortedByDescentingRating = [...books].sort(
+//   (firstBook, secondBook) => secondBook.rating - firstBook.rating
+// );
+
+/* Дополни функцию sortByAscendingBalance(users) так, чтобы она возвращала массив пользователей 
+отсортированный по возрастанию их баланса (свойство balance). */
+// const sortByAscendingBalance = users =>
+//   [...users].sort(
+//     (firstUser, secondUser) => firstUser.balance - secondUser.balance
+//   );
+
+// console.log(sortByAscendingBalance(users));
+
+/* Дополни функцию sortByDescendingFriendCount(users) так, чтобы она возвращала массив пользователей
+ отсортированный по убыванию количества их друзей (свойство friends). */
+// const sortByDescendingFriendCount = users =>
+//   [...users].sort(
+//     (firstUser, secondUser) =>
+//       secondUser.friends.length - firstUser.friends.length
+//   );
+
+// console.log(sortByDescendingFriendCount(users));
+
+/* Дополни код так, чтобы в переменной names получился массив имён авторов в алфавитном порядке, 
+рейтинг книг которых больше значения переменной MIN_BOOK_RATING. */
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   {
+//     title: 'The Dreams in the Witch House',
+//     author: 'Howard Lovecraft',
+//     rating: 8.67,
+//   },
+// ];
+
+// const MIN_BOOK_RATING = 8;
+// // Change code below this line
+
+// const names = books
+//   .filter(book => book.rating > 8)
+//   .sort((firstBook, secondBook) =>
+//     firstBook.author.localeCompare(secondBook.author)
+//   )
+//   .map(book => book.author);
+
+// console.log(names);
+
+/* Дополни функцию getNamesSortedByFriendCount(users) так, чтобы она возвращала массив имён 
+пользователей отсортированный по возрастанию количества их друзей (свойство friends). */
+// const getNamesSortedByFriendCount = users =>
+//   [...users]
+//     .sort(
+//       (firstUser, secondUser) =>
+//         firstUser.friends.length - secondUser.friends.length
+//     )
+//     .map(user => user.name);
+
+// console.log(getNamesSortedByFriendCount(users));
+
+/* Дополни функцию getSortedFriends(users) так, чтобы она возвращала массив уникальных имён друзей 
+(свойство friends) отсортированный по алфавиту. */
+// const getSortedFriends = users => {
+//   return [...users]
+//     .flatMap(user => user.friends)
+//     .filter((friend, index, array) => array.indexOf(friend) === index)
+//     .sort((firstFriend, secondFriend) =>
+//       firstFriend.localeCompare(secondFriend)
+//     );
+// };
+// console.log(getSortedFriends(users));
+
+/* Дополни функцию getTotalBalanceByGender(users, gender) так, чтобы она возвращала общий баланс пользователей 
+(свойство balance), пол которых (свойство gender) совпадает со значением параметра gender. */
+// const getTotalBalanceByGender = (users, gender) => {
+//   return [...users]
+//     .filter(user => user.gender === gender)
+//     .reduce((total, { balance }) => total + balance, 0);
+// };
+
+// console.log(getTotalBalanceByGender(users, 'female'));
+// console.log(getTotalBalanceByGender(users, 'male'));
